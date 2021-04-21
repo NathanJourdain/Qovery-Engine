2d69af5928d10bffaa8093f6d2e217bbb59e4a86
NathanXenoX <75223434+NathanXenoX@users.noreply.github.com>
2021-04-21 16:46:59 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,23 @@
+
+gsap.registerPlugin(ScrollTrigger)
+
+gsap.to(".how-work", {
+    scrollTrigger: ".how-work",
+    opacity : 1,
+    duration : 2
+
+});
+
+gsap.to(".usage", {
+    scrollTrigger: ".usage",
+    opacity : 1,
+    duration : 2
+
+});
+
+gsap.to(".grid", {
+    scrollTrigger: ".grid",
+    opacity : 1,
+    duration : 2
+
+});
